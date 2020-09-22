a8752073b5a870ebab1275f64b94b35ab3fb0826
Saps7 <saptarshisg.2000@gmail.com>
2020-09-22 06:05:58 +0530

    Update, Delete functionalities added and styling

--- routes/students.js
+++ routes/students.js
@@ -44,13 +44,90 @@ router.post('/', async (req, res) => {
   }
 })
 
+//Show Students Route
+router.get('/:id', async (req, res) => {
+  try {
+    const student = await Student.findById(req.params.id).exec()
+    res.render('students/show', { student: student })
+  } catch {
+    res.redirect('/')
+  }
+})
+
+// Edit student Route
+router.get('/:id/edit', async (req, res) => {
+  try {
+    const student = await Student.findById(req.params.id)
+    renderEditPage(res, student)
+  } catch {
+    res.redirect('/')
+  }
+})
+
+// Update student Route
+router.put('/:id', async (req, res) => {
+  let student
+
+  try {
+    student = await Student.findById(req.params.id)
+    student.name = req.body.name
+    student.email = req.body.email
+    student.phone = req.body.phone
+    student.degree = req.body.degree
+    if (req.body.cover != null && req.body.cover !== '') {
+      saveCover(student, req.body.cover)
+    }
+    await student.save()
+    res.redirect(`/students/${student.id}`)
+  } catch {
+    if (student != null) {
+      renderEditPage(res, student, true)
+    } else {
+      redirect('/')
+    }
+  }
+})
+
+// Delete student Page
+router.delete('/:id', async (req, res) => {
+  let student
+  try {
+    student = await Student.findById(req.params.id)
+    await student.remove()
+    res.redirect('/students')
+  } catch {
+    if (student != null) {
+      res.render('students/show', {
+        student: student,
+        errorMessage: 'Could not remove student'
+      })
+    } else {
+      res.redirect('/')
+    }
+  }
+})
+
 async function renderNewPage(res, student, hasError = false) {
+  renderFormPage(res, student, 'new', hasError)
+}
+
+async function renderEditPage(res, student, hasError = false) {
+  renderFormPage(res, student, 'edit', hasError)
+}
+
+async function renderFormPage(res, student, form, hasError = false) {
   try {
     const params = {
       student: student
     }
-    if (hasError) params.errorMessage = 'Error Creating student'
-    res.render('students/new', params)
+    if (hasError) {
+      if (form === 'edit') {
+        params.errorMessage = 'Error Updating student'
+      } else {
+        params.errorMessage = 'Error Creating student'
+      }
+    }
+    res.render(`students/${form}`, params)
   } catch {
     res.redirect('/students')
   }
